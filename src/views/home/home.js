c1f91e79c5f37dab0719a28937ebddc641ae349c
liweili50 <602356374@qq.com>
2018-08-04 17:14:26 +0800

    基础布局

---
+++ src/views/home/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        首页
+      </div>
+    );
+  }
+}
+
+export default Home;
