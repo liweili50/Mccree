c850fc82d64960035a10faa496aa279b69dd657a
liweili50 <602356374@qq.com>
2018-08-06 20:18:12 +0800

    增加响应式展开菜单

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isCollapse: true};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({isCollapse: !this.state.isCollapse});
+  }
   render() {
+    const isCollapse = this.state.isCollapse;
     return (
       <nav className="navbar navbar-toggleable-md">
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          {/* <span className="navbar-toggler-icon"></span> */}
-          =
+        <button onClick={this.handleClick} className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <i className="iconfont">&#xe7f4;</i>
         </button>
         <div className="container">
           <Link to="/" className="navbar-brand">React Website</Link>
-          <div className="collapse navbar-collapse" id="navbarCollapse">
+          <div className={isCollapse ? 'collapse navbar-collapse' : 'navbar-collapse'} id="navbarCollapse">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
                 <Link to="/" className="nav-link">Home <span className="sr-only">(current)</span></Link>
