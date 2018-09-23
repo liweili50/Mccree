179fed2688314afb631844bac2bd23d73214213c
liweili50 <602356374@qq.com>
2018-09-23 15:36:01 +0800

    基础布局

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -4,10 +4,19 @@ import './index.css';
 class Home extends Component {
   render() {
     return (
-      <main>
+      <main className="main">
         <div className="container">
-          <div className="notification">
-            This container is <strong>centered</strong> on desktop.
+          <div className="columns">
+            <div className="column is-8">
+              <p className="bd-notification is-primary">
+                main
+              </p>
+            </div>
+            <div className="column">
+              <p className="bd-notification is-primary">
+                aside
+              </p>
+            </div>
           </div>
         </div>
       </main>
