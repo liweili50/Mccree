eea3a884e0abb4f0da7d41d5df7bf88718ec0d0f
liweili50 <liweili50@163.com>
2019-02-15 17:31:34 +0800

    add tag

--- src/views/tag/index.js
+++ src/views/tag/index.js
@@ -3,13 +3,17 @@ import React, { Component } from "react";
 class Tag extends Component {
   constructor(props) {
     super(props);
-    console.log(props)
+    console.log(props);
   }
-  render() { 
+  render() {
     return (
       <div className="section is-body is-mobile">
         <div className="container">
-            <h5>产品</h5>
+          <div className="columns is-centered">
+            <div className="column is-one-quarter-desktop" >
+                <p className="is-size-5	 has-text-centered">{this.props.match.params.id}</p>
+            </div>
+          </div>
         </div>
       </div>
     );
