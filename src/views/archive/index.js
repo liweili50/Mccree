0c3ed1ed8397b9f3168bfaaac382edc49da97a13
liweili50 <liweili50@163.com>
2019-03-06 21:00:08 +0800

    archive

--- src/views/archive/index.js
+++ src/views/archive/index.js
@@ -1,24 +1,111 @@
 import React, { Component } from "react";
-import DocumentTitle from 'react-document-title'
+import DocumentTitle from "react-document-title";
+import "./index.css";
 
 class Footer extends Component {
   render() {
     return (
-      <DocumentTitle title='My Web App'>
-      <div className="section is-body is-mobile">
-        <div className="container">
-          <aside className="menu">
-            <ul className="menu-list">
+      <DocumentTitle title="归档">
+        <div className="section is-body is-mobile">
+          <div className="container archive-content has-background-white">
+            {/* <aside className="menu"> */}
+            <ul className="">
               <li>
-               
+                <h5 className="is-size-4">2018</h5>
+                <ul className="archive-item">
+                  <li>
+                    <span className="icon">
+                      <i
+                        title="邮箱"
+                        className="czs-square-o"
+                        aria-hidden="true"
+                      />
+                    </span>
+                    <span className="inline-block">2018年10月</span>
+                    <span>Linux编辑文档，习惯性的卡住了！</span>
+                  </li>
+                  <li>
+                  <span className="icon">
+                      <i
+                        title="邮箱"
+                        className="czs-square-o"
+                        aria-hidden="true"
+                      />
+                    </span>
+                    <span>2018年10月</span>
+                    <span>Linux编辑文档，习惯性的卡住了！</span>
+                  </li>
+                  <li>
+                  <span className="icon">
+                      <i
+                        title="邮箱"
+                        className="czs-square-o"
+                        aria-hidden="true"
+                      />
+                    </span>
+                    <span>2018年10月</span>
+                    <span>Linux编辑文档，习惯性的卡住了！</span>
+                  </li>
+                </ul>
               </li>
               <li>
-               
+                <h5 className="is-size-4">2017</h5>
+                <ul className="archive-item">
+                  <li>
+                  <span className="icon">
+                      <i
+                        title="邮箱"
+                        className="czs-square-o"
+                        aria-hidden="true"
+                      />
+                    </span>
+                    <span>2018年10月</span>
+                    <span>Linux编辑文档，习惯性的卡住了！</span>
+                  </li>
+                  <li>
+                  <span className="icon">
+                      <i
+                        title="邮箱"
+                        className="czs-square-o"
+                        aria-hidden="true"
+                      />
+                    </span>
+                    <span>2018年10月</span>
+                    <span>Linux编辑文档，习惯性的卡住了！</span>
+                  </li>
+                  <li>
+                  <span className="icon">
+                      <i
+                        title="邮箱"
+                        className="czs-square-o"
+                        aria-hidden="true"
+                      />
+                    </span>
+                    <span>2018年10月</span>
+                    <span>Linux编辑文档，习惯性的卡住了！</span>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <h5 className="is-size-4">2016</h5>
+                <ul className="archive-item">
+                  <li>
+                  <span className="icon">
+                      <i
+                        title="邮箱"
+                        className="czs-square-o"
+                        aria-hidden="true"
+                      />
+                    </span>
+                    <span>2018年10月</span>
+                    <span>Linux编辑文档，习惯性的卡住了！</span>
+                  </li>
+                </ul>
               </li>
             </ul>
-          </aside>
+            {/* </aside> */}
+          </div>
         </div>
-      </div>
       </DocumentTitle>
     );
   }
