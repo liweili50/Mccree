125bc17bdd41af3d8d8a04d70e50cd06133342bb
liweili50 <liweili50@163.com>
2019-06-03 14:07:09 +0800

    tag

--- src/views/tags/index.js
+++ src/views/tags/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './index.css'
 
-// const tagItems = props.article.tags.map((tag, index) => (
-//   <Link to={'/tags/'+tag} key={index} className="tag">
-//     #{tag}
-//   </Link>
-// ));
+let tags = ['One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','算法','原型链'];
+const tagItems = tags.map((tag, index) => (
+  <Link to={'/tags/'+tag} key={tag} className="tag">
+    {tag}
+  </Link>
+));
 class Tags extends Component {
   render() {
     return (
@@ -15,29 +16,10 @@ class Tags extends Component {
           <div className="tags-container">
             <div className="tags-info">
               <h3>全部标签</h3>
-              <p>目前共计59个标签</p>
+              <p>目前共计{tags.length}个标签</p>
             </div>
             <div className="tags are-medium">
-              <span className="tag">One</span>
-              <span className="tag">Two</span>
-              <span className="tag">Three</span>
-              <span className="tag">Four</span>
-              <span className="tag">Five</span>
-              <span className="tag">Six</span>
-              <span className="tag">Seven</span>
-              <span className="tag">Eight</span>
-              <span className="tag">Nine</span>
-              <span className="tag">Ten</span>
-              <span className="tag">Eleven</span>
-              <span className="tag">Twelve</span>
-              <span className="tag">Thirteen</span>
-              <span className="tag">Fourteen</span>
-              <span className="tag">Fifteen</span>
-              <span className="tag">Sixteen</span>
-              <span className="tag">Seventeen</span>
-              <span className="tag">Eighteen</span>
-              <span className="tag">Nineteen</span>
-              <span className="tag">Twenty</span>
+             {tagItems}
             </div>
           </div>
         </div>
