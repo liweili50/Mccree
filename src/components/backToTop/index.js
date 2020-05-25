ca1c70d9d81458e351566cc08c9112ba8cb81729
Wei Li 李威 <wei.li5.o@MB.local>
2020-05-25 13:58:55 +0800

    fix backToTop

---
+++ src/components/backToTop/index.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import jump from 'jump.js';
+
+export default class BackToTop extends Component {
+  contructor () {
+    this.state({
+      isShow: false
+    })
+    this.getScroll = this.getScroll.bind(this)
+  }
+  
+  UNSAFE_componentWillMount() {
+    this.handleScroll()
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  UNSAFE_componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+  getScroll = (w, top) => {
+    let ret = w[
+      `page${top
+        ? 'Y'
+        : 'X'}Offset`
+    ]
+    const method = `scroll${top
+      ? 'Top'
+      : 'Left'}`
+    if (typeof ret !== 'number') {
+      const d = w.document
+      // ie6,7,8 standard mode
+      ret = d.documentElement[method]
+      if (typeof ret !== 'number') {
+        // quirks mode
+        ret = d.body[method]
+      }
+    }
+    return ret
+  }
+  handleScroll = () => {
+    const showHeight = 400
+    this.setState({
+      isShow: this.getScroll(window, true) > showHeight
+    })
+  }
+
+  scrollToTop (options) {
+    jump(document.body, options)
+  }
+
+  render() {
+    const {
+      className,
+      children,
+      options,
+      showHeight,
+      ...props
+    } = this.props
+    return this.state.isShow
+      ? (<div {...props} className={`jump back-to-top ${className}`} onClick={() => this.scrollToTop(options)}>
+        {children}
+      </div>)
+      : null
+  }
+}
